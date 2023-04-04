081c7bb7be18f74503e3b340d19d8172ac141eb7
drbarrett <drbarrett@cmu.edu>
2023-04-03 23:08:58 -0400

    fixed typo

--- completions/fish_say.fish
+++ completions/fish_say.fish
@@ -5,7 +5,7 @@ complete -x -c fish_say -s i -l inner --exclusive -a '(set_color --print-colors)
 complete -x -c fish_say -s t -l text --exclusive -a '(set_color --print-colors)'
 complete -x -c fish_say -s b -l bubble --exclusive -a '(set_color --print-colors)'
 complete -x -c fish_say -s e -l eye --exclusive -a 'O 0 @ X'
-complete -x -c fish_say -s M -l mouth --exclusive -a '[ L \> /<'
+complete -x -c fish_say -s M -l mouth --exclusive -a '[ L \> \<'
 complete -f -c fish_say -l save
 complete -f -c fish_say -l delete
 
